b541be3b8135b62e9864197d6f8a6036696913b4
Chia-Chun Wei <weicc@cs.nctu.edu.tw Activity Groups Contributed projects Personal p>
2018-09-06 13:14:46 +0800

    ver 106.1-3

--- assets/js/ismylife.js
+++ assets/js/ismylife.js
@@ -1,18 +1,32 @@
 function loadVideoList( pageName ) {
-  const pageNameHtml = pageName + ".html"
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("mainpage").innerHTML =
-      this.responseText;
-    }
-  };
-  xhttp.open("GET", pageNameHtml, true);
-  xhttp.send();
+    const pageNameHtml = pageName + ".html"
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("mainpage").innerHTML =
+            this.responseText;
+        }
+    };
+    xhttp.open("GET", pageNameHtml, true);
+    xhttp.send();
 };
 
 $(document).ready(function() {
     $( ".opener" ).click(function(event) {
-      loadVideoList(event.target.id);
+        loadVideoList(event.target.id);
     });
 });
+
+function openVideo(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
